b7ce4305854a08f026584cc55ab9ad85c682b242
Teddy <tedrandby@gmail.com>
2020-06-11 12:53:56 -0400

    Cards counted: 1422 cards!

--- analyze.js
+++ analyze.js
@@ -7,5 +7,12 @@ var data = getJSON(path.join(__dirname + "/data.json"));
 var call1Data = data["call1"].result;
 
 var result = call1Data.filter(hasHandwritten);
+console.log("total cards in call1:",call1Data.length);
+console.log("handwritten cards in first call:", result.length);
 
-console.log (result.length);
+var count = 0;
+Object.keys(data).forEach(key=> {
+    count += data[key].result.length;
+})
+
+console.log("total card count:",count);
